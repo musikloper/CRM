64146c7d58171e975067a82a6aedc9cb4c03fdfd
chan_ <chan_@DESKTOP-3RLM29C>
2021-07-25 14:15:42 +0900

    UserVO 생성

---
+++ crm_ver1/src/main/java/com/project/crm/user/UserVO.java
@@ -0,0 +1,397 @@
+package com.project.crm.user;
+
+public class UserVO {
+	private int seq;
+	private String user_no;
+	private String user_id;
+	private String email;
+	private String pwd;
+	private String user_type;
+	private String bsn;
+	private String cpn_nm;
+	private String user_nm;
+	private String rep_nm;
+	private String tel;
+	private String rep_tel;
+	private String fax;
+	private String add_tel_1;
+	private String add_tel_2;
+	private String elec_tax_bill_emal;
+	private String bth_day;
+	private String age;
+	private String sex;
+	private String cvsc_eng_no;
+	private String bsn_reg_no;
+	private String frgn_yn;
+	private String mms_rcv;
+	private String evnt_rcv;
+	private String chrg_prsn_id;
+	private String inf_path;
+	private String rsd_reg_no;
+	private String use_lgg;
+	private String time_slot;
+	private String job;
+	private String zip;
+	private String addr;
+	private String addr_dtr;
+	private String intrd_prsn;
+	private String unqe_sbjt;
+	private String tbst;
+	private String reg_dt;
+	private String reg_nm;
+	private String upd_dt;
+	private String upd_nm;
+	private String use_yn;
+	private String del_yn;
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getUser_no() {
+		return user_no;
+	}
+
+	public void setUser_no(String user_no) {
+		this.user_no = user_no;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getUser_type() {
+		return user_type;
+	}
+
+	public void setUser_type(String user_type) {
+		this.user_type = user_type;
+	}
+
+	public String getBsn() {
+		return bsn;
+	}
+
+	public void setBsn(String bsn) {
+		this.bsn = bsn;
+	}
+
+	public String getCpn_nm() {
+		return cpn_nm;
+	}
+
+	public void setCpn_nm(String cpn_nm) {
+		this.cpn_nm = cpn_nm;
+	}
+
+	public String getUser_nm() {
+		return user_nm;
+	}
+
+	public void setUser_nm(String user_nm) {
+		this.user_nm = user_nm;
+	}
+
+	public String getRep_nm() {
+		return rep_nm;
+	}
+
+	public void setRep_nm(String rep_nm) {
+		this.rep_nm = rep_nm;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getRep_tel() {
+		return rep_tel;
+	}
+
+	public void setRep_tel(String rep_tel) {
+		this.rep_tel = rep_tel;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getAdd_tel_1() {
+		return add_tel_1;
+	}
+
+	public void setAdd_tel_1(String add_tel_1) {
+		this.add_tel_1 = add_tel_1;
+	}
+
+	public String getAdd_tel_2() {
+		return add_tel_2;
+	}
+
+	public void setAdd_tel_2(String add_tel_2) {
+		this.add_tel_2 = add_tel_2;
+	}
+
+	public String getElec_tax_bill_emal() {
+		return elec_tax_bill_emal;
+	}
+
+	public void setElec_tax_bill_emal(String elec_tax_bill_emal) {
+		this.elec_tax_bill_emal = elec_tax_bill_emal;
+	}
+
+	public String getBth_day() {
+		return bth_day;
+	}
+
+	public void setBth_day(String bth_day) {
+		this.bth_day = bth_day;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getCvsc_eng_no() {
+		return cvsc_eng_no;
+	}
+
+	public void setCvsc_eng_no(String cvsc_eng_no) {
+		this.cvsc_eng_no = cvsc_eng_no;
+	}
+
+	public String getBsn_reg_no() {
+		return bsn_reg_no;
+	}
+
+	public void setBsn_reg_no(String bsn_reg_no) {
+		this.bsn_reg_no = bsn_reg_no;
+	}
+
+	public String getFrgn_yn() {
+		return frgn_yn;
+	}
+
+	public void setFrgn_yn(String frgn_yn) {
+		this.frgn_yn = frgn_yn;
+	}
+
+	public String getMms_rcv() {
+		return mms_rcv;
+	}
+
+	public void setMms_rcv(String mms_rcv) {
+		this.mms_rcv = mms_rcv;
+	}
+
+	public String getEvnt_rcv() {
+		return evnt_rcv;
+	}
+
+	public void setEvnt_rcv(String evnt_rcv) {
+		this.evnt_rcv = evnt_rcv;
+	}
+
+	public String getChrg_prsn_id() {
+		return chrg_prsn_id;
+	}
+
+	public void setChrg_prsn_id(String chrg_prsn_id) {
+		this.chrg_prsn_id = chrg_prsn_id;
+	}
+
+	public String getInf_path() {
+		return inf_path;
+	}
+
+	public void setInf_path(String inf_path) {
+		this.inf_path = inf_path;
+	}
+
+	public String getRsd_reg_no() {
+		return rsd_reg_no;
+	}
+
+	public void setRsd_reg_no(String rsd_reg_no) {
+		this.rsd_reg_no = rsd_reg_no;
+	}
+
+	public String getUse_lgg() {
+		return use_lgg;
+	}
+
+	public void setUse_lgg(String use_lgg) {
+		this.use_lgg = use_lgg;
+	}
+
+	public String getTime_slot() {
+		return time_slot;
+	}
+
+	public void setTime_slot(String time_slot) {
+		this.time_slot = time_slot;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getAddr_dtr() {
+		return addr_dtr;
+	}
+
+	public void setAddr_dtr(String addr_dtr) {
+		this.addr_dtr = addr_dtr;
+	}
+
+	public String getIntrd_prsn() {
+		return intrd_prsn;
+	}
+
+	public void setIntrd_prsn(String intrd_prsn) {
+		this.intrd_prsn = intrd_prsn;
+	}
+
+	public String getUnqe_sbjt() {
+		return unqe_sbjt;
+	}
+
+	public void setUnqe_sbjt(String unqe_sbjt) {
+		this.unqe_sbjt = unqe_sbjt;
+	}
+
+	public String getTbst() {
+		return tbst;
+	}
+
+	public void setTbst(String tbst) {
+		this.tbst = tbst;
+	}
+
+	public String getReg_dt() {
+		return reg_dt;
+	}
+
+	public void setReg_dt(String reg_dt) {
+		this.reg_dt = reg_dt;
+	}
+
+	public String getReg_nm() {
+		return reg_nm;
+	}
+
+	public void setReg_nm(String reg_nm) {
+		this.reg_nm = reg_nm;
+	}
+
+	public String getUpd_dt() {
+		return upd_dt;
+	}
+
+	public void setUpd_dt(String upd_dt) {
+		this.upd_dt = upd_dt;
+	}
+
+	public String getUpd_nm() {
+		return upd_nm;
+	}
+
+	public void setUpd_nm(String upd_nm) {
+		this.upd_nm = upd_nm;
+	}
+
+	public String getUse_yn() {
+		return use_yn;
+	}
+
+	public void setUse_yn(String use_yn) {
+		this.use_yn = use_yn;
+	}
+
+	public String getDel_yn() {
+		return del_yn;
+	}
+
+	public void setDel_yn(String del_yn) {
+		this.del_yn = del_yn;
+	}
+
+	@Override
+	public String toString() {
+		return "User [seq=" + seq + ", user_no=" + user_no + ", user_id=" + user_id + ", email=" + email + ", pwd="
+				+ pwd + ", user_type=" + user_type + ", bsn=" + bsn + ", cpn_nm=" + cpn_nm + ", user_nm=" + user_nm
+				+ ", rep_nm=" + rep_nm + ", tel=" + tel + ", rep_tel=" + rep_tel + ", fax=" + fax + ", add_tel_1="
+				+ add_tel_1 + ", add_tel_2=" + add_tel_2 + ", elec_tax_bill_emal=" + elec_tax_bill_emal + ", bth_day="
+				+ bth_day + ", age=" + age + ", sex=" + sex + ", cvsc_eng_no=" + cvsc_eng_no + ", bsn_reg_no="
+				+ bsn_reg_no + ", frgn_yn=" + frgn_yn + ", mms_rcv=" + mms_rcv + ", evnt_rcv=" + evnt_rcv
+				+ ", chrg_prsn_id=" + chrg_prsn_id + ", inf_path=" + inf_path + ", rsd_reg_no=" + rsd_reg_no
+				+ ", use_lgg=" + use_lgg + ", time_slot=" + time_slot + ", job=" + job + ", zip=" + zip + ", addr="
+				+ addr + ", addr_dtr=" + addr_dtr + ", intrd_prsn=" + intrd_prsn + ", unqe_sbjt=" + unqe_sbjt
+				+ ", tbst=" + tbst + ", reg_dt=" + reg_dt + ", reg_nm=" + reg_nm + ", upd_dt=" + upd_dt + ", upd_nm="
+				+ upd_nm + ", use_yn=" + use_yn + ", del_yn=" + del_yn + "]";
+	}
+}
