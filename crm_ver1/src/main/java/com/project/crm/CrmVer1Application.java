5c061bc38a2a027c5a861f600cd225fd31022fd1
chan_ <chan_@DESKTOP-3RLM29C>
2021-07-25 14:03:44 +0900

    패키지 생성

---
+++ crm_ver1/src/main/java/com/project/crm/CrmVer1Application.java
@@ -0,0 +1,13 @@
+package com.project.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmVer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmVer1Application.class, args);
+	}
+
+}
