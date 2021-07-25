51524c4e21b3fc9fcddeafda344ddb2275d81184
musikloper <musikloper0601@gmail.com>
2021-07-25 13:49:57 +0900

    crm init

---
+++ crm_ver1/src/test/java/com/project/crm/CrmVer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmVer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
